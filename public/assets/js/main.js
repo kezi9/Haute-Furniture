0a60c23b89947550fc7219f009679805982d895f
kezi9 <kmeliana@myseneca.ca>
2024-07-12 01:21:26 -0400

    html, js, scss

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,19 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    // Glide.JS
-    new Glide('.glide').mount();
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     // Glide.JS
+//     new Glide('.glide').mount();
 
-    // AOS
+//     // AOS
+//     AOS.init();
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Glide.js for the carousel
+    new Glide('.glide').mount();
+  
+    // Initialize AOS for animations when you scroll
     AOS.init();
-});
+  
+    // Initialize Parsley.js to check the form when you try to submit
+    $('#contact-form').parsley();
+  });
+  
