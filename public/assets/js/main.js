55b6df3fbb530f9161dd67cea10c1b3e83ca9fcd
kezi9 <kmeliana@myseneca.ca>
2024-07-15 00:35:01 -0400

    Change AOS

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
   };
   new Glide('.glide', config).mount();
 
-  // Initialize Lightbox
+  // Lightbox
   if (typeof lightbox !== 'undefined') {
     lightbox.option({
       'resizeDuration': 200,
